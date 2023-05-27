d39ab4e116342644c8e3411a691a4fed38b4a2b0
Huzaifa <Huzaifaashfaque@users.noreply.github.com>
2023-05-27 23:21:43 +0530

    First Commit

---
+++ Review_Scrapper/scrapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def aboutus(request):
+    return render(request,"aboutus.html")
+
+def review(request):
+    return render(request,"review.htm")
+
+def donate(request):
+    return render(request,"donate.htm")
