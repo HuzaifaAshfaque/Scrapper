d6eb8087b2f59672de00072cf41712820b1ecb71
Huzaifa <Huzaifaashfaque@users.noreply.github.com>
2023-06-02 15:01:15 +0530

    modifye

--- Review_Scrapper/scrapp/urls.py
+++ Review_Scrapper/scrapp/urls.py
@@ -5,7 +5,7 @@ from scrapp import views
 urlpatterns = [
     path("",views.index,name='home'),
     path("aboutus",views.aboutus,name='aboutus'),
-    path("review",views.review,name='reviews'),
+    path("reviews",views.review,name='reviews'),
     path("donate",views.donate,name='donate')
 
 ]
